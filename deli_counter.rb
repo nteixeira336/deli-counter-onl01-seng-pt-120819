d0f60368b6b9f6aa44326a927964550518918d7a
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-18 15:35:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+katz_deli= [ ]
+def line(katz_deli)
+  if katz_deli== [ ]
+    puts "The line is currently empty."
+  else 
+   person=1 
+   string= "The line is currently:"
+    while person>=1 do 
+    string+="#{person}. katz_deli[0]"
+    person+=1 
+    end 
+  end 
+end
+
+def take_a_number(katz_deli, name)
+
+end 
+
+def now_serving(katz_deli)
+  
+end 
+
+
